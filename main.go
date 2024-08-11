91cab5510f8e64e85aca06bec98b9c6e42366be1
Shivom Sharma <shars119@mcmaster.ca>
2024-08-11 17:44:59 -0500

    Fixing files in commit

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func booxSearch(cmd *cobra.Command, args []string) {
 	var searchType string
 	var source string
 
+	queryBoox()
 	searchTypePrompt := &survey.Select{
 		Message: "Select a category to search from",
 		Options: options,
